591256663b944c22cb4a878bf4036f330c346f2f
mrslow <animal2k@gmail.com>
2019-10-29 14:37:22 +0300

    Изменил архитектуру. Поправил тесты.

--- aiosigmasms.py
+++ aiosigmasms.py
@@ -1,56 +1,51 @@
 import aiohttp
 import logging
 
-__all__ = ['Client']
+__all__ = ['Client', 'SigmaClientError']
+
+
+class SigmaClientError(Exception):
+    pass
 
 
 class Client:
 
     BASE_URL = 'http://online.sigmasms.ru/api'
 
-    def __init__(self, username=None, password=None, sender=None):
+    def __init__(self, username, password, sender):
         self.username = username
         self.password = password
         self.sender = sender
-        self.token = None
 
-    async def auth(self):
-        if not (self.username and self.password):
-            raise ValueError('Login and password are required')
-        if not self.token:
-            await self._set_token()
-
-    async def _set_token(self):
-        url = f'{self.BASE_URL}/login'
+    async def login(self):
+        if not (self.username or self.password):
+            raise SigmaClientError('Username and password are required')
         data = {'username': self.username, 'password': self.password}
-        async with aiohttp.ClientSession() as client:
-            async with client.post(url, json=data) as resp:
-                json = await resp.json()
-                self.token = json['token'] if not json.get('error') else None
-
-    async def _request(self, method, url, **kwargs):
-        await self.auth()
-        if self.token:
-            async with aiohttp.ClientSession() as client:
-                async with client.request(method, url, **kwargs) as resp:
-                    return await resp.json()
-
-    async def send_message(self, recipient, message, type):
-        url = f'{self.BASE_URL}/sendings'
-        payload = {
-            'recipient': recipient,
-            'type': type,
-            'payload': {
-                'sender': self.sender,
-                'text': message
-            }
-        }
-        headers = {'Authorization': self.token}
-        resp = await self._request('POST', url, json=payload, headers=headers)
+        resp = await self._request('POST', 'login',  json=data)
         return resp
 
-    async def check_status(self, message_id):
-        url = f'{self.BASE_URL}/sendings/{message_id}'
-        headers = {'Authorization': self.token}
-        resp = await self._request('GET', url, headers=headers)
+    async def _request(self, method, endpoint, **kwargs):
+        url = f'{self.BASE_URL}/{endpoint}'
+        async with aiohttp.ClientSession() as client:
+            async with client.request(method, url, **kwargs) as resp:
+                return await resp.json()
+
+    async def send_message(self, rcpt, msg, msg_type, token):
+        payload = {
+            'recipient': rcpt,
+            'type': msg_type,
+            'payload': {
+                'sender': self.sender,
+                'text': msg
+            }
+        }
+        headers = {'Authorization': token}
+        resp = await self._request('POST', 'sendings', json=payload,
+                                   headers=headers)
+        return resp
+
+    async def check_status(self, message_id, token):
+        headers = {'Authorization': token}
+        endpoint = f'sendings/{message_id}'
+        resp = await self._request('GET', endpoint, headers=headers)
         return resp
--- test_aiosigmasms.py
+++ test_aiosigmasms.py
@@ -8,16 +8,18 @@ pytestmark = [pytest.mark.asyncio]
 
 @pytest.fixture
 def valid_client():
-    client = aiosigmasms.Client()
-    client.username = 'user'
-    client.password = 'pass'
-    client.sender = 'sender'
+    client = aiosigmasms.Client('user', 'password', 'sender')
     return client
 
 
 @pytest.fixture
 def invalid_client():
-    return aiosigmasms.Client()
+    return aiosigmasms.Client(None, None, None)
+
+
+@pytest.fixture
+def token():
+    return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
 
 
 @pytest.fixture
@@ -26,49 +28,47 @@ def mock():
         yield mock
 
 
-async def test_auth(valid_client, invalid_client, mock):
-    response = ('{"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9", "id": "1",'
-                '"username": "username"}')
+async def test_login(valid_client, invalid_client, mock, token):
+    response = '{"token": "' + token + '", "id": "1", "username": "username"}'
     mock.post('http://online.sigmasms.ru/api/login', status=200, body=response)
-    await valid_client.auth()
-    assert 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' == valid_client.token
+    resp = await valid_client.login()
+    assert 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' == resp['token']
 
-    with pytest.raises(ValueError):
-        await invalid_client.auth()
+    with pytest.raises(aiosigmasms.SigmaClientError,
+                       match='Username and password are required'):
+        await invalid_client.login()
 
 
-async def test_send_message(valid_client, mock):
-    valid_client.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
+async def test_send_message(valid_client, mock, token):
     response1 = ('{"id": "aca68fa3", "recipient": "+79999999999",'
                  '"status": "pending", "error": null}')
-    headers = {'Authorization': valid_client.token}
+    headers = {'Authorization': token}
     mock.post('http://online.sigmasms.ru/api/sendings', headers=headers,
               body=response1)
-    r = await valid_client.send_message('+79999999999', 'test', 'sms')
+    r = await valid_client.send_message('+79999999999', 'test', 'sms', token)
     assert r['error'] is None
 
     response2 = ('{"id": "aca68fa3", "recipient": "+79999999999",'
                  '"status": "failed", "error": "text"}')
     mock.post('http://online.sigmasms.ru/api/sendings', headers=headers,
               body=response2)
-    r = await valid_client.send_message('+79999999999', 'test', 'sms')
+    r = await valid_client.send_message('+79999999999', 'test', 'sms', token)
     assert 'text' == r['error']
 
 
-async def test_check_status(valid_client, mock):
-    valid_client.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
+async def test_check_status(valid_client, mock, token):
     _id = "aca68fa3"
     response1 = ('{"id": "aca68fa3", "recipient": "+79999999999",'
                  '"state": {"status": "seen", "error": null}}')
-    headers = {'Authorization': valid_client.token}
+    headers = {'Authorization': token}
     mock.get(f'http://online.sigmasms.ru/api/sendings/{_id}', headers=headers,
              body=response1)
-    r = await valid_client.check_status(_id)
+    r = await valid_client.check_status(_id, token)
     assert r['state']['error'] is None
 
     response2 = ('{"id": "aca68fa3", "recipient": "+79999999999",'
                  '"state": {"status": "failed", "error": "text"}}')
     mock.get(f'http://online.sigmasms.ru/api/sendings/{_id}', headers=headers,
              body=response2)
-    r = await valid_client.check_status(_id)
+    r = await valid_client.check_status(_id, token)
     assert 'text' == r['state']['error']
